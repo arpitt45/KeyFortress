6712e9703a3d1bdb457fea66c5151ebb00b21b7c
arpitt45 <arpitkashyap903@gmail.com>
2024-03-20 01:42:38 +0530

    Completed Manager.jsx, Add icons

--- src/Components/Manager.jsx
+++ src/Components/Manager.jsx
@@ -1,6 +1,44 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useRef } from "react";
 
 const Manager = () => {
+    const ref = useRef()
+    const [form, setform] = useState({site:"",username:"", password:""})
+    const [passwordArray, setpasswordArray] = useState([])
+  
+    useEffect(() => {
+        let passwords = localStorage.getItem("passwords")
+        let passwordArray;
+        if(passwords){
+           setpasswordArray(JSON.parse(passwords))
+        }
+    }, [])
+    
+
+
+   const showPassword = () =>{
+     alert("show the password");
+     if(ref.current.src.includes("icons/eye-crossed.png")){
+         ref.current.src = "icons/visibility.png"
+     }
+     else{
+        ref.current.src = "icons/eye-crossed.png"
+     }
+   }
+
+   const savePassword = () =>{
+    setpasswordArray([...passwordArray, form])
+    localStorage.setItem("passwords", JSON.stringify([...passwordArray, form]))
+    console.log(passwordArray)
+    
+   }
+
+   const handleForm = (e) =>{
+      setform({...form, [e.target.name]: e.target.value})
+   }
+
+
+
   return (
     <div>
       <div className="relative h-full w-full bg-slate-50">
@@ -16,25 +54,38 @@ const Manager = () => {
         </div>
       </div>
       <div className=" mycontainer w-1/2 -mt-4 flex flex-col p-4 text-black gap-6 items-center">
-        <input
-          className="rounded-full border border-yellow-400  p-80 py-1"
-          type="text"
+
+        <input onChange={handleForm}
+          className="rounded-full border border-yellow-400  p-80 py-1" name="site"
+          type="text" placeholder="Enter website URL"
+           value={form.site}
         />
+
         <div className="flex w-full justify-between gap-6">
-          <input
+
+          <input onChange={handleForm}
             className="rounded-full border border-yellow-400 p-24 py-1"
             type="text"
-            name=""
-            id=""
+            name="username"
+            id="" placeholder="Enter Username"
+              value={form.username}
           />
-          <input
+
+          <div className="relative">
+          <input  onChange={handleForm}
             className="rounded-full border border-yellow-400 p-24 py-1"
             type="text"
-            name=""
-            id=""
+            name="password"
+            id="" placeholder="Enter Password" value={form.password}
+ 
           />
+          <span className="absolute right-1 top-0 " onClick={showPassword}>
+          <img ref={ref} width={36} src="icons/visibility.png" alt="eye" className="p-2" />
+          </span>
+          </div>
+
         </div>
-        <button className="flex justify-center items-center bg-yellow-400 hover:bg-yellow-200 rounded-full px-4 py-2 w-fit gap-2 border-2 border-slate-950">
+        <button onClick={savePassword} className="flex justify-center items-center bg-yellow-400 hover:bg-yellow-200 rounded-full px-4 py-2 w-fit gap-2 border-2 border-slate-500 font-semibold">
         <lord-icon
           src="https://cdn.lordicon.com/jgnvfzqg.json"
           trigger="hover"
@@ -42,6 +93,51 @@ const Manager = () => {
         ></lord-icon>
          Add Password</button>
       </div>
+
+      <div className="passwords mycontainer">
+        
+       
+
+<div className="relative overflow-x-auto container px-460 py-16 mx-auto -m-20 mr-40">
+        <h2 className="font-bold text-2xl py-3 gap-4">Your Passwords</h2>
+        {passwordArray.length === 0 && <div>No passwords to show</div> }
+
+         {passwordArray.length !=0 &&<table className="w-full text-sm text-left rtl:text-right text-white dark:text-gray-400 rounded-xl overflow-hidden">
+        <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-slate-950 dark:text-yellow-300">
+            <tr>
+                <th scope="col" className="px-6 py-3">
+                    SITE
+                </th>
+                <th scope="col" className="px-6 py-3">
+                   USERNAME
+                </th>
+                <th scope="col" className="px-6 py-3">
+                    PASSWORD
+                </th>
+            
+            </tr>
+        </thead>
+        <tbody>
+            {passwordArray.map((item, index)=>{
+         return <tr key={index} className="bg-white border-b dark:bg-slate-600 dark:border-gray-700">
+                <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                   <a href={item.site} target="_blank">{item.site}</a> 
+                </th>
+                <td className="px-6 py-4">
+                   {item.username}
+                </td>
+                <td className="px-6 py-4">
+                    {item.password}
+                </td>   
+            </tr>
+          })}
+        </tbody>
+    </table> }
+</div>
+
+
+      </div>
+
     </div>
   );
 };
