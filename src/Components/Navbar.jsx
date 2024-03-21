4f35b2cf2b8b847e581334f867b3b86a0f957253
arpitt45 <arpitkashyap903@gmail.com>
2024-03-22 02:06:20 +0530

    Add portfolio to navbar

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-
+import { TERipple } from "tw-elements-react";
 const Navbar = () => {
   return (
     <nav className="bg-slate-950 text-white  ">
@@ -10,15 +10,19 @@ const Navbar = () => {
         </div>
         <ul>
           <li className="flex gap-4">
-            <a className="hover:font-bold" href="/">
-              Home
+            <a className="hover:font-bold" target="_blank" href="https://arpitt45.github.io/Portfolio/">
+        <TERipple>
+        </TERipple>
+          <button
+            type="button"
+            className="inline-block rounded border-2 border-white px-6 pb-[6px] pt-2 text-xs font-medium uppercase leading-normal text-white transition duration-150 ease-in-out hover:border-yellow-500 hover:bg-yellow-500 hover:bg-opacity-10 hover:text-yellow-500 focus:border-white focus:text-white focus:outline-none focus:ring-0 active:border-white active:text-white dark:border-white dark:text-white dark:hover:border-white dark:hover:bg-white dark:hover:bg-opacity-40 dark:hover:text-white dark:focus:border-white dark:focus:text-white dark:active:border-white dark:active:text-white"
+            > PORTFOLIO
+            </button>
+
+
+
             </a>
-            <a className="hover:font-bold" href="#">
-              Home
-            </a>
-            <a className="hover:font-bold" href="#">
-              Home
-            </a>
+            
           </li>
         </ul>
       </div>
